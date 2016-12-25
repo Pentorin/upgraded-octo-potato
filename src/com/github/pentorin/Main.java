ed7ba52a52c355b6ecce8b86eb1f375b754046eb
Aaron Belz <belzaaron@gmail.com>
2016-12-24 20:37:19 -0500

    :sparkles: New fancy Main with a print
    Essentially added basic things to get the project going (IDE files etc.)

---
+++ src/com/github/pentorin/Main.java
@@ -0,0 +1,11 @@
+package com.github.pentorin;
+
+/**
+ * @author aaronbelz
+ * @version 24 Dec 2016
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
